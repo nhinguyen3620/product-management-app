6e959cad8ab555a862312363e0a6f3ad4de15936
Nhi Nguyen <nhinguyen@Nhis-MacBook-Pro.local>
2022-07-17 21:07:06 -0400

    finalized app

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { filter, Subscription } from "rxjs";
+import { ProductService } from "./product.services";
+import { IProduct } from "./products";
+
+@Component({
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
+})
+
+export class ProductListComponent implements OnInit, OnDestroy {
+    constructor(private productService: ProductService) {
+        this.productService = productService;
+    }
+
+    pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin = 2;
+    showImage: boolean = false;
+    errorMessage: string = '';
+    sub!: Subscription;
+
+    private _listFilter: string = '';
+
+    get listFilter(): string {
+        return this._listFilter;
+    }
+
+    set listFilter(value: string) {
+        this._listFilter = value;
+        console.log("in setter:", value);
+        this.filteredProducts = this.performFilter(value);
+    }
+
+    filteredProducts: IProduct[] = [];
+
+    products: IProduct[] = [
+      
+    ];
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) => 
+            product.productName.toLocaleLowerCase().includes(filterBy));
+    }
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void {
+        this.sub = this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error: err => this.errorMessage = err
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.sub.unsubscribe;
+    }
+
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List: ' + message;
+    }
+}
